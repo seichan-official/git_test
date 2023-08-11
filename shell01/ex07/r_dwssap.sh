53308c57d88169bf05e8aa1c76a2fdd0d006717e
seichan-official <109450437+seichan-official@users.noreply.github.com>
2023-08-11 12:11:36 +0900

    shell01

---
+++ shell01/ex07/r_dwssap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FT_LINE1=1
+FT_LINE2=15
+
+cat /etc/passwd | \
+  sed '/^#/d' | \
+  awk 'NR % 2 == 0' | \
+  cut -d: -f1 | \
+  rev | \
+  sort -r | \
+  sed -n "${FT_LINE1},${FT_LINE2}p" | \
+  tr '\n' ', ' | \
+  sed 's/, $/./'
